c25444f4efafa7428ec938d521e0a4ae4f0667f5
Brandon(Suin) Park <Suin.Park@tlgcohort.com>
2023-02-22 08:22:32 -0800

    Lab 5.2 add static imports

--- Lab05.2/src/DerivedDateTimeTest.java
+++ Lab05.2/src/DerivedDateTimeTest.java
@@ -12,10 +12,13 @@
  * For the purposes of this lab, we give you a few wildcarded imports,
  * so you can use the classes in these packages without having to worry about importing them.
  */
-import java.time.*;
-import java.time.format.*;
-import java.time.temporal.TemporalAdjusters;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+import static java.time.DayOfWeek.*; // now, you can say MONDAY instead of DayOfWeek.MONDAY
 import static java.time.temporal.TemporalAdjusters.*;
 
 class DerivedDateTimeTest {
@@ -36,7 +39,7 @@ class DerivedDateTimeTest {
     /**
      * TASK: new American presidents are often measured by their performance during the first 100 days in office.
      * Inauguration Day 2017 is Jan 20.  When is this president's 100-day deadline?
-     *
+     * <p>
      * RESULT:
      */
     public static void testPresidentsFirst100Days() {
@@ -47,7 +50,7 @@ class DerivedDateTimeTest {
      * TASK: Certain times of the year seem to have more birthdays than other times.
      * Some believe that this is because more babies are conceived during certain times of year.
      * Below, assume a due date 38 weeks after conception, during a non-leap-year.
-     *
+     * <p>
      * RESULT:
      */
     public static void testPopularBirthdays() {
@@ -60,7 +63,7 @@ class DerivedDateTimeTest {
      * TASK: you've saved diligently your whole life and plan to retire as soon as
      * you can take distributions from your 401(k) penalty-free, which is when you turn 59 1/2.
      * When will / did you retire?
-     *
+     * <p>
      * RESULT:
      */
     public static void testEarlyRetirement() {
@@ -71,22 +74,25 @@ class DerivedDateTimeTest {
      * TASK: when was Labor Day during the year you were born?
      * Note: start with a LocalDate equal to your birthday.
      * Note: Labor Day is the first Monday in Sept.
-     *
+     * <p>
      * RESULT:
      */
     public static void testLaborDay() {
-        // TODO
+        LocalDate sept1 = LocalDate.of(1966,9,1);
+        LocalDate laborDay = sept1.with(nextOrSame(MONDAY));
+        System.out.println(laborDay);
+        System.out.println(laborDay.getDayOfWeek());
     }
 
     /**
      * TASK: Election Day in the United States is defined as the Tuesday immediately after the 1st Monday in November.
      * When is Election Day 2024?
-     *
+     * <p>
      * RESULT:
      */
     public static void testElectionDay() {
-        LocalDate nov1 = LocalDate.of(2024,11,1);
-        LocalDate election = nov1.with(TemporalAdjusters.nextOrSame(DayOfWeek.MONDAY)).plusDays(1);
+        LocalDate nov1 = LocalDate.of(2024, 11, 1);
+        LocalDate election = nov1.with(nextOrSame(MONDAY)).plusDays(1);
         System.out.println(election);
         System.out.println(election.getDayOfWeek());
     }
@@ -96,13 +102,13 @@ class DerivedDateTimeTest {
      * They are planning their 50th wedding anniversary, and would like to throw a big party.
      * If their anniversary does not fall on a Saturday, they'd like to have their party the following Saturday.
      * What is the date of the party?
-     *
+     * <p>
      * RESULT:
      */
     public static void testAnniversary() {
-        LocalDate wedding = LocalDate.of(1969,6,6);
+        LocalDate wedding = LocalDate.of(1969, 6, 6);
         LocalDate anniversary = wedding.plusYears(50);
-        LocalDate party = anniversary.with(TemporalAdjusters.nextOrSame(DayOfWeek.SATURDAY));
+        LocalDate party = anniversary.with(nextOrSame(SATURDAY));
         System.out.println(anniversary);
         System.out.println(anniversary.getDayOfWeek());
         System.out.println(party);
