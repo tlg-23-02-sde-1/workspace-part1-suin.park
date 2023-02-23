b99aca1b6076a299e0a0cefe2a23cc52f14a6e82
Brandon(Suin) Park <Suin.Park@tlgcohort.com>
2023-02-23 08:38:36 -0800

    Lab 10.2 Complete

---
+++ Lab10.1/src/gov/irs/IllegalWageException.java
@@ -0,0 +1,25 @@
+package gov.irs;
+
+/*
+ * Application custom exception - and it is checked.
+ */
+
+public class IllegalWageException extends Exception {
+
+    public IllegalWageException() {
+        super();
+    }
+
+    public IllegalWageException(String message) {
+        super(message);
+    }
+
+    public IllegalWageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public IllegalWageException(Throwable cause) {
+        super(cause);
+    }
+
+}
